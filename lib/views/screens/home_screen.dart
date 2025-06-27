9eb99ce39798d0c12f1df0bc3bbdc6fcf2e6ee37
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-06-27 18:40:40 +0500

    Showing events on screen

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../models/model.dart';
+import '../../view_models/app_states.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -12,7 +15,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late GetEventsViewModel getEventsViewModel;
-
 
   @override
   void initState() {
@@ -26,48 +28,66 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   callViewModels() {
-   getEventsViewModel.getEvents();
+    getEventsViewModel.getEvents();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Events',
+        appBar: AppBar(
+          title: const Text(
+            'Events',
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: ListView.separated(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20.w,
-          vertical: 20.h,
-        ),
-        itemBuilder: (context, index) {
-          return SizedBox(
-            height: 100.h,
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.r),
-                color: Colors.white38
-              ),
-              child: Column(
-                children: [
-                  eventHeader(heading: 'Name', value: 'value'),
-                  eventHeader(heading: 'Description', value: 'value'),
-                  eventHeader(heading: 'Date/Time', value: 'value'),
-                  eventHeader(heading: 'Event Type', value: 'value'),
-                ],
-              ),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return 10.verticalSpace;
-        },
-        itemCount: 10,
-      ),
-    );
+        body: BlocBuilder<GetEventsViewModel, AppState>(
+          builder: (context, state) {
+            if (state is SuccessState<List<Event>>) {
+              return ListView.separated(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.w,
+                  vertical: 20.h,
+                ),
+                itemBuilder: (context, index) {
+                  Event event = state.data[index];
+                  return SizedBox(
+                    height: 100.h,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.r),
+                          color: Colors.white38),
+                      child: Column(
+                        children: [
+                          eventHeader(
+                            heading: 'Name',
+                            value: event.name ?? '',
+                          ),
+                          eventHeader(
+                            heading: 'Description',
+                            value: event.description ?? '',
+                          ),
+                          eventHeader(
+                            heading: 'Date/Time',
+                            value: event.dateTime.toString(),
+                          ),
+                          eventHeader(
+                            heading: 'Event Type',
+                            value: event.type ?? '',
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return 10.verticalSpace;
+                },
+                itemCount: state.data.length,
+              );
+            }
+            return Container();
+          },
+        ));
   }
 
   eventHeader({required String heading, required String value}) {
@@ -78,7 +98,7 @@ class _HomeScreenState extends State<HomeScreen> {
           heading,
         ),
         Text(
-            value,
+          value,
         ),
       ],
     );
