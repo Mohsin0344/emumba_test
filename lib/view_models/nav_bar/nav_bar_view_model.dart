a3c43a1c9a7c5e1d1356c95fa59537f24f68b932
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-07-01 20:50:48 +0500

    added titles and icons in viewmodel for easy access

--- lib/view_models/nav_bar/nav_bar_view_model.dart
+++ lib/view_models/nav_bar/nav_bar_view_model.dart
@@ -1,8 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 
 
 class NavBarViewModel extends Cubit<int> {
   NavBarViewModel() : super(0);
+
+  List navBarItemTitles = [
+    'List View',
+    'Calendar View',
+  ];
+
+  List navBarIcons = [
+    Icons.list,
+    Icons.calendar_month,
+  ];
 
   changeTap({
     required int tabIndex,
