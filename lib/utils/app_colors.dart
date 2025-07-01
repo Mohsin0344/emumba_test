fd9f9f624de6dbe008451423cf2643b5c64b52f7
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-07-02 02:22:49 +0500

    added app colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -6,4 +6,20 @@ class AppColors {
   static const Color blackColor = Colors.black;
   static const Color disabledColor = Colors.grey;
   static const Color redColor = Colors.red;
+  static const Color textFieldBackgroundColor = Color(0xFFF7F7F7);
+  static const Color hintTextColor = Color(0xFFA2A2A2);
+  static const Color appBarLeadingIconColor = Color(0xff5A5A5A);
+  static const Color shimmerBaseColor = Color(0xFFA2A2A2);
+  static const Color shimmerHighlightColor = Colors.white;
+  static const Color greenColor = Colors.green;
+  static const Color orangeColor = Colors.orange;
+  static const Color purpleColor = Color(0xff2E2739);
+  static const Color lightTextColor = Color(0xffDBDBDF);
+  static const Color elevatedButtonColor = Color(0xff61C3F2);
+  static const Color overviewColor = Color(0xff8F8F8F);
+  static const Color unSelectedItemColor = Color(0xff827D88);
+  static const Color lightGrey = Color(0xffF6F6F6);
+  static const Color notAvailableColor = Color(0xffA6A6A6);
+  static const Color selectedColor = Color(0xffCD9D0F);
+  static const Color vipColor = Color(0xff564CA3);
 }
