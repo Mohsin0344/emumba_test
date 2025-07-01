aaafb8f7136c4d6891d2b58f42f9babc963826aa
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-07-02 00:02:54 +0500

    Added events list widget

--- lib/views/screens/home/events/events_list_view_screen.dart
+++ lib/views/screens/home/events/events_list_view_screen.dart
@@ -1,6 +1,8 @@
+import 'package:emumba_test/views/screens/home/events/events_list.dart';
 import 'package:emumba_test/views/widgets/app_sliver_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../utils/app_colors.dart';
 import '../../../../utils/app_fonts.dart';
 import '../../../../view_models/events/get_events_view_model.dart';
@@ -14,7 +16,12 @@ class EventsListViewScreen extends StatefulWidget {
 
 class _EventsListViewScreenState extends State<EventsListViewScreen> {
   late GetEventsViewModel getEventsViewModel;
-
+  List<String> eventTypes = [
+    'All',
+    'Event',
+    'Out of Office',
+    'Task',
+  ];
 
   @override
   void initState() {
@@ -22,15 +29,46 @@ class _EventsListViewScreenState extends State<EventsListViewScreen> {
     initViewModels();
   }
 
-  initViewModels() {
-
-  }
+  initViewModels() {}
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        AppSliverBar(),
+        const AppSliverBar(),
+        SliverPadding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20.w,
+            vertical: 20.h,
+          ),
+          sliver: SliverToBoxAdapter(
+            child: Wrap(
+              spacing: 10.h,
+              children: List.generate(
+                eventTypes.length,
+                    (index) => DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.r,),
+                    color: AppColors.primaryColor,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 12.w,
+                      vertical: 12.h,
+                    ),
+                    child: Text(
+                      eventTypes[index],
+                      style: AppFonts.bodyFont(
+                        color: AppColors.secondaryColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        EventsList(),
       ],
     );
   }
