63913fe2eefc1766f04c4d2e4ed78dc153986d8a
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-07-02 02:22:43 +0500

    added required providers in main

--- lib/providers/bloc_providers.dart
+++ lib/providers/bloc_providers.dart
@@ -1,7 +1,10 @@
+import 'package:emumba_test/view_models/events/delete_event_view_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../data/events_repository.dart';
+import '../view_models/events/create_event_view_model.dart';
 import '../view_models/events/get_events_view_model.dart';
+import '../view_models/events/update_event_view_model.dart';
 import '../view_models/nav_bar/nav_bar_view_model.dart';
 
 class BlocProviders {
@@ -13,6 +16,21 @@ class BlocProviders {
     ),
     BlocProvider<NavBarViewModel>(
       create: (context) => NavBarViewModel(),
+    ),
+    BlocProvider<CreateEventViewModel>(
+      create: (context) => CreateEventViewModel(
+        EventRepository(),
+      ),
+    ),
+    BlocProvider<DeleteEventViewModel>(
+      create: (context) => DeleteEventViewModel(
+        EventRepository(),
+      ),
+    ),
+    BlocProvider<UpdateEventViewModel>(
+      create: (context) => UpdateEventViewModel(
+        EventRepository(),
+      ),
     )
   ];
 }
